3d4466c8f61fe616090b4d57737f04abecd4af95
omarayad1 <omarayad1@gmail.com>
2015-05-13 00:19:31 +0200

    added a register data route

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
+from parser import register_file
+
 app = Flask(__name__)
 
 
@@ -6,5 +8,12 @@ app = Flask(__name__)
 def nothing():
     return render_template('PipelineSimulator.html')
 
+@app.route("/registerData", methods=['GET'])
+def get_register_data():
+	data = []
+	for i in xrange(32):
+		data.append(register_file.load_from_reg(i))
+	print data
+	return jsonify({"data":data})
 if __name__ == "__main__":
 	app.run(debug=True)
