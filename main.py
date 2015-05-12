3b56387206876f4423132b9f7ebf0f4c83f16f11
omarayad1 <omarayad1@gmail.com>
2015-05-13 00:38:38 +0200

    added a route to get memory data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, jsonify
-from parser import register_file
+from parser import register_file, data_memory
 
 app = Flask(__name__)
 
@@ -13,7 +13,15 @@ def get_register_data():
 	data = []
 	for i in xrange(32):
 		data.append(register_file.load_from_reg(i))
-	print data
 	return jsonify({"data":data})
+
+@app.route("/memoryData", methods=['GET'])
+def get_memory_data():
+	data = []
+	for i in xrange(32):
+		data.append(data_memory.load_word(i))
+	return jsonify({"data":data})
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
