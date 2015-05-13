c51c99172b465178aecea481f1d45748dfed3f6f
Youssef/Yousif Rehab <loufis@aucegypt.edu>
2015-05-13 10:08:32 +0300

    Update sim.py
    fixed some bugs in the file

--- sim.py
+++ sim.py
@@ -3,25 +3,34 @@ from native import pipe_line
 from parser import register_file, data_memory, pc
 
 inst_mem = 0
-counter = 0
 stall=False
-cycle=1
 pipe=0
 
 def fill_inst_mem(file):
 	code = parser.get_file(file)
 	code = parser.split_to_lines(code)
 
-	global data
+	global inst_mem
 	global pipe
+	global stall
+	global cycle
+	counter = 0
+	stall=False
 	inst_mem = parser.fill_instr_mem(code)
-	'Im not sure about the following line, the pipe_line constructor is supposed to take a pc pointer Im not sure how to do that'
 	pipe = pipe_line.pipe_line(pc)
+	
+	
+	ret = pipe.fetch(inst_mem.getInst(pc.get()))
+	pipe.write()
+       	pipe.decode()
+        pipe.execute()
+        pipe.access()
+        stall = pipe.upline()
+	
 	return (inst_mem is None)
 
 def next_step():
-	global data
-	global counter
+	global inst_mem
 	global stall
 	global cycle
 	'ret is False if the simulator did not fetch a new instruction but is still running'
@@ -29,15 +38,16 @@ def next_step():
 	'ret contains the name of the new instruction that was added to the pipeline otherwise'
 	ret = False
 
-	if !pipe.empty():
-		if !stall:
+	if not pipe.empty():
+		if not stall:
 			ret = pipe.fetch(inst_mem.getInst(pc.get()))
+			pc.increment()
 		pipe.write()
-        pipe.decode()
-        pipe.execute()
-        pipe.access()
-        stall = pipe.upline()
-    else:
-    	ret = None
+        	pipe.decode()
+        	pipe.execute()
+        	pipe.access()
+        	stall = pipe.upline()
+    	else:
+    		ret = None
 
-    return ret
+    	return ret
